049a71e52b611d265e54d9a26077a677b163c55a
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-01-25 10:26:47 -0800

    set up server and routes

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const router = require('./router.js');
+
+const app = express();
+const port = 3002;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/api', router);
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.use(express.static(path.join(__dirname, '../public')));
