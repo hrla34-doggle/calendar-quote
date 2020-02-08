2624078fff86d0209142ba1bfe8ca595c319b7b0
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-02-08 10:03:53 -0800

    refactor Month component to stateless functional

--- client/src/components/Month.jsx
+++ client/src/components/Month.jsx
@@ -1,54 +1,47 @@
 import React from 'react';
 
-export default class Month extends React.Component {
-  constructor(props) {
-    super(props);
+const Month = (props) => {
+  const { trip, renderTrailingBlanks, renderLeadingBlanks, dates, days, stringifyPrice, month, dayHeaders, numMonth, handleSummaryandSelect, highlightedDate, highlightedDates } = props;
+  const { price, discounted } = trip;
 
-    this.state = {
-    };
-  }
-
-  render() {
-    const { trip, renderTrailingBlanks, renderLeadingBlanks, dates, days, stringifyPrice, month, dayHeaders, numMonth, handleSummaryandSelect, highlightedDate, highlightedDates } = this.props;
-    const { price, discounted } = trip;
-
-    return (
-      <div className="AK-container-month">
-        <div className="AK-container-header-month">
-          <span className="AK-text-month">{month} 2020</span>
+  return (
+    <div className="AK-container-month">
+      <div className="AK-container-header-month">
+        <span className="AK-text-month">{month} 2020</span>
+      </div>
+      <div className="AK-container-weeks">
+        <div className="AK-header-days">
+          {dayHeaders.map((day) => <div className="AK-container-day"><div className="AK-day">{day}</div></div>)}
         </div>
-        <div className="AK-container-weeks">
-          <div className="AK-header-days">
-            {dayHeaders.map((day) => <div className="AK-container-day"><div className="AK-day">{day}</div></div>)}
-          </div>
-          <div className="AK-container-dates">
-            {renderLeadingBlanks()}
-            {days().map((day, index) => {
-              if (dates.indexOf(index + 1) !== -1) {
-                return (<div className={highlightedDate[0] === numMonth && highlightedDate[1] === (index + 1) ? "AK-container-date-departure AK-container-date-departure-selected" : "AK-container-date-departure"}
-                  data-index={index}
-                  data-nummonth={numMonth}
-                  onClick={handleSummaryandSelect}>
-                  <div className="AK-text-date">{day}</div>
-                  <div className={discounted ? "AK-date-pricing-discounted" : "AK-date-pricing"}>{stringifyPrice(price)}</div>
-                </div>
-                );
-              }
-              var highlighted = false;
-              highlightedDates.forEach((date) => {
-                if (date[0] === numMonth && date[1] === index + 1) {
-                  highlighted = true;
-                }
-              });
-              return (<div className={highlighted ? "AK-container-date AK-container-date-highlighted" : "AK-container-date"} key={index}>
+        <div className="AK-container-dates">
+          {renderLeadingBlanks()}
+          {days().map((day, index) => {
+            if (dates.indexOf(index + 1) !== -1) {
+              return (<div className={highlightedDate[0] === numMonth && highlightedDate[1] === (index + 1) ? "AK-container-date-departure AK-container-date-departure-selected" : "AK-container-date-departure"}
+                data-index={index}
+                data-nummonth={numMonth}
+                onClick={handleSummaryandSelect}>
                 <div className="AK-text-date">{day}</div>
+                <div className={discounted ? "AK-date-pricing-discounted" : "AK-date-pricing"}>{stringifyPrice(price)}</div>
               </div>
               );
-            })}
-            {renderTrailingBlanks()}
-          </div>
+            }
+            var highlighted = false;
+            highlightedDates.forEach((date) => {
+              if (date[0] === numMonth && date[1] === index + 1) {
+                highlighted = true;
+              }
+            });
+            return (<div className={highlighted ? "AK-container-date AK-container-date-highlighted" : "AK-container-date"} key={index}>
+              <div className="AK-text-date">{day}</div>
+            </div>
+            );
+          })}
+          {renderTrailingBlanks()}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Month;
